c203489b734acd6f2a1c08c34673b573149105fe
Karimsalum <karimrsalum@gmail.com>
2022-10-19 11:39:54 -0300

    n

--- src/main/java/com/portfolio/karim/Controller/CEducacion.java
+++ src/main/java/com/portfolio/karim/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://karimfrontapp.web.app")
+@CrossOrigin(origins = {"https://karimfrontapp.web.app", "http://localhost:4200"})
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
